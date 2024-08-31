fc332bbbae850ffef70c2d0664ec1bb6cd4656f3
Adriano Martinez <asb.martinez@alumno.um.edu.ar>
2024-08-30 22:44:59 -0300

    rook

--- pieces/rook.py
+++ pieces/rook.py
@@ -29,25 +29,22 @@ class Rook(Piece):
 
         return True
    
-    def _check_vertical_path(self, from_x, from_y, to_y):
-     step = 1 if to_y > from_y else -1
-     for y in range(from_y + step, to_y, step):
-        piece = self.__board__.get_piece(from_x, y)
-        if piece is not None:
-            return False
-     return True
-
-    def _check_horizontal_path(self, from_x, from_y, to_x):
-     step = 1 if to_x > from_x else -1
-     for x in range(from_x + step, to_x, step):
-        piece = self.__board__.get_piece(x, from_y)
-        if piece is not None:
-            return False
-     return True
-
-    def _check_path(self, from_x, from_y, to_x, to_y, vertical):
-     if vertical:
-        return self._check_vertical_path(from_x, from_y, to_y)
+     
+    def _check_path(self, from_x, from_y, to_x, to_y):
+     if from_x == to_x:  # Movimiento vertical
+        step = 1 if to_y > from_y else -1
+        for y in range(from_y + step, to_y, step):
+            piece = self.__board__.get_piece(from_x, y)
+            if piece is not None:
+                return False
+     elif from_y == to_y:  # Movimiento horizontal
+        step = 1 if to_x > from_x else -1
+        for x in range(from_x + step, to_x, step):
+            piece = self.__board__.get_piece(x, from_y)
+            if piece is not None:
+                return False
      else:
-        return self._check_horizontal_path(from_x, from_y, to_x)
+        raise ValueError("El movimiento no es ni vertical ni horizontal.")
+    
+     return True
 
